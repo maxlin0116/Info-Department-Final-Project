6ef7bf81fc9aec17436340462db9bfcaec9dbaa1
maxlin0116 <maxlin0116@gmail.com>
2026-05-08 10:17:51 +0800

    fix vite.config.ts type problem

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ const resolveFromRoot = (relativePath: string) =>
 function figmaAssetResolver() {
   return {
     name: 'figma-asset-resolver',
-    resolveId(id) {
+    resolveId(id: string) {
       if (id.startsWith('figma:asset/')) {
         const filename = id.replace('figma:asset/', '')
         return resolveFromRoot(`./src/assets/${filename}`)
